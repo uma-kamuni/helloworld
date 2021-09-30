3c7af58b538fad804735f8a62aa201981054d9ef
uma-kamuni <91591263+uma-kamuni@users.noreply.github.com>
2021-09-29 22:40:39 -0700

    java program

---
+++ Helloworld.java
@@ -0,0 +1,12 @@
+
+public class Helloworld{
+        public static void main(String[] argv) {
+                while(true){
+                        system.out.println("Hello world");
+                                try{
+                                Thread.sleep(60000);
+                        }
+                         catch(Exception ex){}
+                }
+        }
+}
