f046d756735a7cd42b3cf8a4047ba23f4144bb5f
uma-kamuni <kamuniumaramesh@gmail.com>
2021-09-30 12:17:30 -0700

    java code change

--- Helloworld.java
+++ Helloworld.java
@@ -3,7 +3,7 @@ public class Helloworld{
         public static void main(String[] argv) {
                 while(true){
                         system.out.println("Hello world");
-                        system.out.println("uma kamuni");
+                        system.out.println("uma");
                         break;
                 }
         }
